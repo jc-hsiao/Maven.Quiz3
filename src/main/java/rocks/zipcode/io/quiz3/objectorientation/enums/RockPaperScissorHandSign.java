48f788162b8b737cb425802266408fd88e099b8f
Leila Hsiao <lsiao@One-of-the-Zipcodes-MacBookssssss.local>
2020-03-09 12:14:26 -0400

    save progress on part 5

--- src/main/java/rocks/zipcode/io/quiz3/objectorientation/enums/RockPaperScissorHandSign.java
+++ src/main/java/rocks/zipcode/io/quiz3/objectorientation/enums/RockPaperScissorHandSign.java
@@ -4,13 +4,27 @@ package rocks.zipcode.io.quiz3.objectorientation.enums;
  * @author leon on 09/12/2018.
  */
 public enum RockPaperScissorHandSign {
-    ADD_ENUMERATIONS_HERE;
+    ROCK,
+    PAPER,
+    SCISSOR;
 
     public RockPaperScissorHandSign getWinner() {
+        if(this.equals(ROCK))
+            return PAPER;
+        else if(this.equals(PAPER))
+            return SCISSOR;
+        else if(this.equals(SCISSOR))
+            return ROCK;
         return null;
     }
 
     public RockPaperScissorHandSign getLoser() {
+        if(this.equals(ROCK))
+            return SCISSOR;
+        else if(this.equals(PAPER))
+            return ROCK;
+        else if(this.equals(SCISSOR))
+            return PAPER;
         return null;
     }
 }
