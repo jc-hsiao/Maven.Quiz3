3c3e8d466dc3ff83c4591d8e2e57ae004d9cb1fe
Leila Hsiao <lsiao@One-of-the-Zipcodes-MacBookssssss.local>
2020-03-09 11:35:20 -0400

    save progress on pigLatin

--- src/main/java/rocks/zipcode/io/quiz3/fundamentals/PigLatinGenerator.java
+++ src/main/java/rocks/zipcode/io/quiz3/fundamentals/PigLatinGenerator.java
@@ -5,6 +5,38 @@ package rocks.zipcode.io.quiz3.fundamentals;
  */
 public class PigLatinGenerator {
     public String translate(String str) {
-        return null;
+        String[] sArr = str.split(" ");
+        StringBuilder result = new StringBuilder();
+        for(String s : sArr){
+            result.append(parse(s)).append(" ");
+        }
+        return result.toString().substring(0,result.length()-1);
+    }
+
+    public String parse(String str){
+        if( isV(str.charAt(0)) )
+            return str+"way";
+        else{
+            int trim = 0;
+            String tail = "";
+            for(char c :str.toCharArray()){
+                if(!isV(c)){
+                    tail += c+"";
+                    trim++;
+                }else
+                    break;
+            }
+
+            String res = str.substring(trim);
+            return res+tail+"ay";
+        }
+    }
+
+    public boolean isV (char c){
+        return c == 'a' || c == 'A' ||
+                c == 'e' || c == 'E' ||
+                c == 'i' || c == 'I' ||
+                c == 'o' || c == 'O' ||
+                c == 'u' || c == 'U';
     }
 }
