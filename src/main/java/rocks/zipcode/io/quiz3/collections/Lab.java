06823d1e277b722e8e04e720806e553e41b409b8
Leila Hsiao <lsiao@One-of-the-Zipcodes-MacBookssssss.local>
2020-03-09 11:11:20 -0400

    save progress

--- src/main/java/rocks/zipcode/io/quiz3/collections/Lab.java
+++ src/main/java/rocks/zipcode/io/quiz3/collections/Lab.java
@@ -4,21 +4,32 @@ package rocks.zipcode.io.quiz3.collections;
  * @author leon on 10/12/2018.
  */
 public class Lab {
+    String labName;
+    LabStatus stat;
+
     public Lab() {
         this(null);
     }
 
     public Lab(String labName) {
+        this.labName = labName;
+        stat = LabStatus.INCOMPLETE;
     }
 
     public String getName() {
-        return null;
+        return labName;
     }
 
     public void setStatus(LabStatus labStatus) {
+        stat = labStatus;
     }
 
     public LabStatus getStatus() {
-        return null;
+        return stat;
+    }
+
+    @Override
+    public String toString() {
+        return labName;
     }
 }
--- src/main/java/rocks/zipcode/io/quiz3/collections/Student.java
+++ src/main/java/rocks/zipcode/io/quiz3/collections/Student.java
@@ -1,5 +1,6 @@
 package rocks.zipcode.io.quiz3.collections;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -7,24 +8,51 @@ import java.util.List;
  */
 public class Student {
 
+    List<Lab> labs;
+
     public Student() {
-        this(null);
+        this(new ArrayList<>());
     }
 
     public Student(List<Lab> labs) {
+        this.labs = labs;
     }
 
     public Lab getLab(String labName) {
+        for(Lab l :labs){
+            if(l.labName.equals(labName))
+                return l;
+        }
         return null;
     }
 
     public void setLabStatus(String labName, LabStatus labStatus) {
+        if(labs.size()==0)
+            throw new UnsupportedOperationException();
+
+        getLab(labName).setStatus(labStatus);
     }
 
     public void forkLab(Lab lab) {
+        labs.add(0,lab);
+        lab.setStatus(LabStatus.PENDING);
     }
 
     public LabStatus getLabStatus(String labName) {
-        return null;
+//        if(getLab(labName) == null)
+//            return LabStatus.INCOMPLETE;
+//        else
+            return getLab(labName).getStatus();
+    }
+
+
+    @Override
+    public String toString() {
+        StringBuilder s = new StringBuilder();
+        for (Lab l:labs) {
+            //s.insert(0,l.labName+" > "+l.getStatus()+"\n");
+            s.append(l.labName).append(" > ").append(l.getStatus()).append("\n");
+        }
+        return s.toString().substring(0,s.length()-1);
     }
 }
